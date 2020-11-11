b57ada188462bf510e04b0b91b159cbf420bc416
serhiirodikov <sergiy1998373@gmail.com>
2020-11-11 17:45:40 +0200

    added template

--- dtApi/src/app/speciality/isnum.validators.ts
+++ dtApi/src/app/speciality/isnum.validators.ts
@@ -2,7 +2,7 @@ import { FormControl } from '@angular/forms'
 
 export class IsNumValidators {
     static isNum(control: FormControl): { [key: string]: boolean } {
-        let value = parseInt(control.value)
+        const value = parseInt(control.value)
         if (!Number.isInteger(value)) {
             return { isNum: true }
         }
