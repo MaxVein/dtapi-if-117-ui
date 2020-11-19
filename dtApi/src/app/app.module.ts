1d00e7e7b9f9ff19f18539f2b2486827a1faa2fc
Serhii Yaroshenko <datextdk@gmail.com>
2020-11-19 10:00:03 +0200

    resolved conflicts

--- dtApi/src/app/app.module.ts
+++ dtApi/src/app/app.module.ts
@@ -6,24 +6,15 @@ import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AngularMaterialModule } from './shared/modules/angular-material/angular-material.module'
-<<<<<<< HEAD
-import { SpecialityModule } from './speciality/speciality.module'
 
-import { DashboardModule } from './modules/dashboard/dashboard.module'
 import { LoginModule } from './modules/login/login.module'
 import { StudentProfileModule } from './modules/student-profile/student-profileModule/student-profile.module'
-=======
->>>>>>> 3aa30a5fbe3b8c402bb0e0a547c7c3fc3b7be335
-
-import { GroupsModule } from './modules/groups/groups.module'
 import { SharedModule } from './shared/modules/shared.module'
 import { AppComponent } from './app.component'
-
 import { SpecialityModule } from './modules/admin/speciality/speciality.module'
 import { GroupsModule } from './modules/admin/groups/groups.module'
 import { TestModule } from './modules/admin/test/test.module'
 import { DashboardModule } from './modules/admin/dashboard/dashboard.module'
-import { LoginComponent } from './modules/login/login.component'
 
 @NgModule({
     declarations: [AppComponent],
