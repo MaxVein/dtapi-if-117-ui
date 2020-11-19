a74df39434f69e71456a775bdd6466aa7cca2428
Serhii Yaroshenko <datextdk@gmail.com>
2020-11-19 19:17:58 +0200

    fix app modules file

--- dtApi/src/app/app.module.ts
+++ dtApi/src/app/app.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from '@angular/core'
-
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module'
-<<<<<<< HEAD
 import { AngularMaterialModule } from './shared/modules/angular-material/angular-material.module'
 
 import { LoginModule } from './modules/login/login.module'
-import { StudentProfileModule } from './modules/student-profile/student-profileModule/student-profile.module'
+import { StudentPageModule } from './modules/student-profile/student-profileModule/student-profile.module'
 import { SharedModule } from './shared/modules/shared.module'
 import { AppComponent } from './app.component'
 import { SpecialityModule } from './modules/admin/speciality/speciality.module'
@@ -18,34 +17,23 @@ import { DashboardModule } from './modules/admin/dashboard/dashboard.module'
 
 @NgModule({
     declarations: [AppComponent],
-=======
-import { SharedModule } from './shared/modules/shared.module'
-
-import { AppComponent } from './app.component'
-import { NotFoundPageComponent } from './shared/components/not-found-page/not-found-page.component'
-
-@NgModule({
-    declarations: [AppComponent, NotFoundPageComponent],
->>>>>>> b961fe13060c8a4f46ff84d37515026e691e81a3
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+        ReactiveFormsModule,
         HttpClientModule,
         AppRoutingModule,
-<<<<<<< HEAD
         AngularMaterialModule,
         TestModule,
         GroupsModule,
         SpecialityModule,
         DashboardModule,
         LoginModule,
-        StudentProfileModule,
-=======
->>>>>>> b961fe13060c8a4f46ff84d37515026e691e81a3
+        StudentPageModule,
         SharedModule,
     ],
     providers: [],
-    exports: [],
+    exports: [AngularMaterialModule],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
