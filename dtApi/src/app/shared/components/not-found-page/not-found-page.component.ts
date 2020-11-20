19236ed1b460ee566773576e4d05dca72d592c93
olehyaremyn7 <oleh_yaremyn@ukr.net>
2020-11-20 20:57:50 +0200

    Fix

--- dtApi/src/app/shared/components/not-found-page/not-found-page.component.ts
+++ dtApi/src/app/shared/components/not-found-page/not-found-page.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
     selector: 'app-not-found-page',
     templateUrl: './not-found-page.component.html',
     styleUrls: ['./not-found-page.component.scss'],
 })
-export class NotFoundPageComponent {}
+export class NotFoundPageComponent implements OnInit {
+    link: string
+
+    ngOnInit(): void {
+        const role = localStorage.getItem('role')
+        if (role === 'admin') {
+            this.link = '/admin/dashboard'
+        } else {
+            this.link = '/student'
+        }
+    }
+}
