d2ca8cb1780f1d24dd306de6db0735c147d0e0b5
Serhii Yaroshenko <datextdk@gmail.com>
2020-11-20 17:30:57 +0200

    fixed modules import

--- dtApi/src/app/modules/login/login.module.ts
+++ dtApi/src/app/modules/login/login.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
-import { ReactiveFormsModule } from '@angular/forms'
 
 import { LoginComponent } from './login.component'
-import { AngularMaterialModule } from '../../shared/modules/angular-material/angular-material.module'
+import { SharedModule } from '../../shared/modules/shared.module'
 
 @NgModule({
     declarations: [LoginComponent],
     imports: [
         CommonModule,
-        ReactiveFormsModule,
-        AngularMaterialModule,
+        SharedModule,
         RouterModule.forChild([{ path: '', component: LoginComponent }]),
     ],
     exports: [LoginComponent],
--- dtApi/src/app/modules/student-profile/student-page/student-page.module.ts
+++ dtApi/src/app/modules/student-profile/student-page/student-page.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
-import { ReactiveFormsModule } from '@angular/forms'
 
-import { AngularMaterialModule } from 'src/app/shared/modules/angular-material/angular-material.module'
+import { SharedModule } from 'src/app/shared/modules/shared.module'
 import { StudentPageComponent } from './student-page.component'
 
 @NgModule({
     declarations: [StudentPageComponent],
     imports: [
         CommonModule,
-        AngularMaterialModule,
-        ReactiveFormsModule,
+        SharedModule,
         RouterModule.forChild([
             { path: 'student-page', component: StudentPageComponent },
         ]),
