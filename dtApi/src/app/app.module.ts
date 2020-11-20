7b946c6b79e519c61d47937a89819a8f031fbac5
Serhii Yaroshenko <datextdk@gmail.com>
2020-11-20 18:14:54 +0200

    remove unused imports

--- dtApi/src/app/app.module.ts
+++ dtApi/src/app/app.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http'
-import { ReactiveFormsModule } from '@angular/forms'
-
 import { AppRoutingModule } from './app-routing.module'
-import { AngularMaterialModule } from './shared/modules/angular-material/angular-material.module'
 import { SharedModule } from './shared/modules/shared.module'
-import { SpecialityModule } from './modules/admin/speciality/speciality.module'
-import { GroupsModule } from './modules/admin/groups/groups.module'
-import { StudentPageModule } from './modules/student-profile/student-page/student-page.module'
+
 import { NotFoundPageComponent } from './shared/components/not-found-page/not-found-page.component'
 import { AppComponent } from './app.component'
 
@@ -17,19 +12,13 @@ import { AppComponent } from './app.component'
     declarations: [AppComponent, NotFoundPageComponent],
     imports: [
         BrowserModule,
-        StudentPageModule,
         BrowserAnimationsModule,
-        ReactiveFormsModule,
         HttpClientModule,
         AppRoutingModule,
-        AngularMaterialModule,
-        GroupsModule,
-        SpecialityModule,
-        StudentPageModule,
         SharedModule,
     ],
     providers: [],
-    exports: [AngularMaterialModule],
+    exports: [],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
