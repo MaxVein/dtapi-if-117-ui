19c0bfef3f80d7c9c2dbe49b15d4b1698cddd676
Stolyar100 <olejer555@gmail.com>
2020-11-22 20:09:23 +0200

    added modal form

--- dtApi/src/app/modules/admin/faculties/faculties.module.ts
+++ dtApi/src/app/modules/admin/faculties/faculties.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router'
 
 import { FacultiesListComponent } from './faculties-list/faculties-list.component'
 import { SharedModule } from '../../../shared/modules/shared.module'
+import { ModalFormComponent } from './modal-form/modal-form.component'
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component'
 
 const routes: Routes = [{ path: '', component: FacultiesListComponent }]
 
 @NgModule({
-    declarations: [FacultiesListComponent],
+    declarations: [FacultiesListComponent, ModalFormComponent, ConfirmDialogComponent,],
     imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
     exports: [FacultiesListComponent],
 })
