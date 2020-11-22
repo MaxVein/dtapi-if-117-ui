dbd31a456307db0b2972bb1e1415b9c5d5348802
Stolyar100 <olejer555@gmail.com>
2020-11-22 18:24:43 +0200

    finished faculties list component

--- dtApi/src/app/modules/admin/faculties/faculties-list/faculties-list.component.ts
+++ dtApi/src/app/modules/admin/faculties/faculties-list/faculties-list.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core'
+
+import { MatPaginator } from '@angular/material/paginator'
+import { MatDialog, MatDialogRef } from '@angular/material/dialog'
+import { MatTableDataSource } from '@angular/material/table'
+
+import { Faculty } from '../../../../shared/interfaces/interfaces'
+import { ApiService } from '../../../../shared/services/api.service'
+import { ModalFormComponent } from '../modal-form/modal-form.component'
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component'
+import { DialogService } from '../dialog.service'
 
 @Component({
-  selector: 'app-faculties-list',
-  templateUrl: './faculties-list.component.html',
-  styleUrls: ['./faculties-list.component.scss']
+    selector: 'app-faculties-list',
+    templateUrl: './faculties-list.component.html',
+    styleUrls: ['./faculties-list.component.scss'],
 })
 export class FacultiesListComponent implements OnInit {
+    dataSource = new MatTableDataSource<Faculty>()
+    displayedColumns: string[] = ['id', 'name', 'description', 'buttons']
+    fileNameDialogRef: MatDialogRef<ModalFormComponent>
+    confirmDialogRef: MatDialogRef<ConfirmDialogComponent>
 
-  constructor() { }
+    @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private apiService: ApiService,
+        private dialogService: DialogService
+    ) {}
 
+    ngOnInit(): void {
+        this.getFaculty()
+    }
+    ngAfterViewInit(): void {
+        this.dataSource.paginator = this.paginator
+    }
+
+    getFaculty(): void {
+        this.apiService.getEntity('Faculty').subscribe(
+            (response: Faculty[]) => (this.dataSource.data = response),
+            (error) => {
+                this.apiService.snackBarOpen()
+            }
+        )
+    }
+
+    openModal(data?: Faculty[]): void {
+        this.dialogService
+            .createModal(data ? data : '')
+            .afterClosed()
+            .subscribe(
+                (res) => (res ? this.getFaculty() : ''),
+                (error) => {
+                    this.apiService.snackBarOpen()
+                }
+            )
+    }
+
+    editFaculty(elem: Faculty[]): void {
+        this.openModal(elem)
+    }
+
+    deleteFaculty(data: Faculty): void {
+        this.dialogService
+            .openConfirmDialog(data)
+            .afterClosed()
+            .subscribe(
+                (res) => {
+                    if (res === 'ok') {
+                        this.dataSource.data = this.dataSource.data.filter(
+                            (Faculty) =>
+                                Faculty.faculty_id !== data.faculty_id
+                        )
+                    }
+                },
+                (error) => {
+                    this.apiService.snackBarOpen()
+                }
+            )
+    }
 }
