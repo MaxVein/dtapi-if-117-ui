14857ac5e37c37c2a42b9bfdffc446053d2ff548
serhiirodikov <sergiy1998373@gmail.com>
2020-11-22 02:23:08 +0200

    created logged guard

---
+++ dtApi/src/app/shared/guards/logged.guard.ts
@@ -0,0 +1,30 @@
+import { Route } from '@angular/compiler/src/core'
+import { Injectable } from '@angular/core'
+import { CanActivate, Router } from '@angular/router'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+import { ApiService } from 'src/app/modules/admin/speciality/api.service'
+import { AuthService } from 'src/app/modules/login/services/auth.service'
+
+@Injectable({
+    providedIn: 'root',
+})
+export class LoggedGuard implements CanActivate {
+    constructor(private apiService: AuthService, private router: Router) {}
+    canActivate(
+        route: Route
+    ): Observable<boolean> | Promise<boolean> | boolean {
+        return this.apiService.isLogged().pipe(
+            map((data: any) => {
+                if (data.response === 'non logged') {
+                    return true
+                } else {
+                    const goTo = data.roles.includes('admin')
+                        ? 'admin'
+                        : 'student'
+                    this.router.navigate([goTo])
+                }
+            })
+        )
+    }
+}
