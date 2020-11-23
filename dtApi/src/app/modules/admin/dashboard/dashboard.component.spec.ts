f0e5fa5eac919635f3889ddb2fa7f3dd3c450dca
Sebastian <sebi.kvak@gmail.com>
2020-11-23 13:06:44 +0200

    upd:angular

--- dtApi/src/app/modules/admin/dashboard/dashboard.component.spec.ts
+++ dtApi/src/app/modules/admin/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,6 @@
 import { LayoutModule } from '@angular/cdk/layout'
 import { NoopAnimationsModule } from '@angular/platform-browser/animations'
-import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
 import { MatGridListModule } from '@angular/material/grid-list'
@@ -13,20 +13,22 @@ describe('DashboardComponent', () => {
     let component: DashboardComponent
     let fixture: ComponentFixture<DashboardComponent>
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [DashboardComponent],
-            imports: [
-                NoopAnimationsModule,
-                LayoutModule,
-                MatButtonModule,
-                MatCardModule,
-                MatGridListModule,
-                MatIconModule,
-                MatMenuModule,
-            ],
-        }).compileComponents()
-    }))
+    beforeEach(
+        waitForAsync(() => {
+            TestBed.configureTestingModule({
+                declarations: [DashboardComponent],
+                imports: [
+                    NoopAnimationsModule,
+                    LayoutModule,
+                    MatButtonModule,
+                    MatCardModule,
+                    MatGridListModule,
+                    MatIconModule,
+                    MatMenuModule,
+                ],
+            }).compileComponents()
+        })
+    )
 
     beforeEach(() => {
         fixture = TestBed.createComponent(DashboardComponent)
