9ab731ef0122c0977da4972c896debd31272ff20
Oleh <58546051+olehyaremyn7@users.noreply.github.com>
2020-12-21 22:24:49 +0200

    [TESTING/UPDATE/FIX/REFACTOR] feature/students

--- dtApi/src/app/modules/admin/Messages.ts
+++ dtApi/src/app/modules/admin/Messages.ts
@@ -1,27 +1,4 @@
-// Admin Module Shared Messages //
-export const titleErrorMessage = 'Помилка';
-export const errorMessage = 'Сталася помилка. Спробуйте знову';
-export const closeError = 'Закрито через помилку';
-export const cancelErrorMessage = 'Скасовано';
-export const closeMessageE = 'Закрито';
-
 // Students Module Messages //
-export const uploadStudentsMessage = 'Студентів завантажено';
-export const notStudentsMessage = 'У вибраній групі відсутні студенти';
-export const addStudentMessage = 'Студента додано';
-export const updateStudentMessage = 'Дані студента оновлено';
-export const transferStudentMessage = 'Студента переведено';
-export const studentRemoveMessage = 'Студента видалено';
-export const getUpdateErrorMessage =
-    'Сталася помилка при отриманні даних студента. Спробуйте знову';
-export const getFacultyErrorMessage =
-    'Сталася помилка при списку факультетів. Спробуйте знову';
-export const getGroupErrorMessage =
-    'Сталася помилка при списку груп. Спробуйте знову';
-export const transferStudentErrorMessage =
-    'Сталася помилка при переведенні студента. Спробуйте знову';
-export const notViewStudentData = 'Немає даних';
-export const notStudentDataMessage = 'Немає даних про вибраного студента';
 export const studentsTableColumns = [
     'id',
     'gradebook_id',
@@ -29,17 +6,84 @@ export const studentsTableColumns = [
     'actions',
 ];
 
-export function removeStudentMessage(
-    firstname: string,
-    lastname: string
-): string {
-    return `Видалити студента ${firstname} ${lastname}?`;
-}
+type Students =
+    | 'extrasError'
+    | 'upload'
+    | 'notStudents'
+    | 'getError'
+    | 'modalError'
+    | 'modalErrClose'
+    | 'modalCancel'
+    | 'modalClose'
+    | 'add'
+    | 'update'
+    | 'transfer'
+    | 'remove'
+    | 'confirmRemove'
+    | 'viewDataError'
+    | 'notViewData'
+    | 'removeError'
+    | 'viewError'
+    | 'transferError'
+    | 'getGroupsError'
+    | 'getFacultyError'
+    | 'createError'
+    | 'updateError'
+    | 'formInvalid'
+    | 'newStudentError';
 
-export function updateCreateErrorMessage(update: boolean): string {
-    if (update) {
-        return 'Сталася помилка під час оновлення даних студента. Спробуйте знову';
-    } else {
+export function studentsMessages(
+    students: Students,
+    firstname?: string,
+    lastname?: string
+): string {
+    if (students === 'extrasError') {
+        return 'Сталася помилка! Відсутні дані для отримання студентів! Спробуйте знову';
+    } else if (students === 'upload') {
+        return 'Студентів завантажено';
+    } else if (students === 'notStudents') {
+        return 'У вибраній групі відсутні студенти';
+    } else if (students === 'getError') {
+        return 'Сталася помилка! Не вдалося завантажити студентів даної групи! Спробуйте знову';
+    } else if (students === 'modalError') {
+        return 'Помилка';
+    } else if (students === 'modalErrClose') {
+        return 'Закрито через помилку';
+    } else if (students === 'modalCancel') {
+        return 'Скасовано';
+    } else if (students === 'modalClose') {
+        return 'Закрито';
+    } else if (students === 'add') {
+        return 'Студента додано';
+    } else if (students === 'update') {
+        return 'Дані студента оновлено';
+    } else if (students === 'transfer') {
+        return 'Студента переведено';
+    } else if (students === 'remove') {
+        return 'Студента видалено';
+    } else if (students === 'confirmRemove') {
+        return `Видалити студента ${firstname} ${lastname}?`;
+    } else if (students === 'viewDataError') {
+        return 'Немає даних';
+    } else if (students === 'notViewData') {
+        return 'Немає даних про вибраного студента';
+    } else if (students === 'removeError') {
+        return 'Сталася помилка! Не вдалося видалити вибраного студента! Спробуйте знову';
+    } else if (students === 'viewError') {
+        return 'Сталася помилка при отриманні даних студента. Спробуйте знову';
+    } else if (students === 'transferError') {
+        return 'Сталася помилка при переведенні студента. Спробуйте знову';
+    } else if (students === 'getGroupsError') {
+        return 'Сталася помилка при списку груп. Спробуйте знову';
+    } else if (students === 'getFacultyError') {
+        return 'Сталася помилка при списку факультетів. Спробуйте знову';
+    } else if (students === 'createError') {
         return 'Сталася помилка під час створення студента. Спробуйте знову';
+    } else if (students === 'updateError') {
+        return 'Сталася помилка під час оновлення даних студента. Спробуйте знову';
+    } else if (students === 'formInvalid') {
+        return 'Сталася помилка! Заповніть форму відповідно до вимог і спробуйте знову';
+    } else if (students === 'newStudentError') {
+        return 'Сталася помилка при формуванні даних студента! Спробуйде знову';
     }
 }
